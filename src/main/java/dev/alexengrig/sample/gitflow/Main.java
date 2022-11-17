b8f63706b0f07e765109fc4c958223ef355d1986
Grig Alex <alexengrigdev@gmail.com>
2022-11-17 09:15:55 +0300

    Fix init

--- src/main/java/dev/alexengrig/sample/gitflow/Main.java
+++ src/main/java/dev/alexengrig/sample/gitflow/Main.java
@@ -1,4 +1,6 @@
-package dev.alexengrig.sample.gitflow;public class Main {
+package dev.alexengrig.sample.gitflow;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
